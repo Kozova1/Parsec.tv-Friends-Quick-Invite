9a08ffe868c06e7b1afb37500f06de3d77f4e6f8
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-11-13 19:47:25 +0200

    Add files via upload

---
+++ Parsec-Friends-Quick-Invite/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Parsec_Friends_Quick_Invite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txt_sIDToConnect = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.txt_YourSID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Args = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.Location = new System.Drawing.Point(12, 286);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(141, 34);
+            this.btn_Create.TabIndex = 0;
+            this.btn_Create.Text = "Create Invitation + installation File";
+            this.btn_Create.UseVisualStyleBackColor = true;
+            this.btn_Create.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(267, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Invite yourself and others to his quick hosted server";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(144, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Invite them to your server";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txt_sIDToConnect
+            // 
+            this.txt_sIDToConnect.Location = new System.Drawing.Point(231, 104);
+            this.txt_sIDToConnect.Name = "txt_sIDToConnect";
+            this.txt_sIDToConnect.Size = new System.Drawing.Size(100, 20);
+            this.txt_sIDToConnect.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server ID to Connect to:";
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(191, 297);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(57, 23);
+            this.btn_Connect.TabIndex = 5;
+            this.btn_Connect.Text = "Connect to this server ID";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // txt_YourSID
+            // 
+            this.txt_YourSID.Location = new System.Drawing.Point(385, 104);
+            this.txt_YourSID.Name = "txt_YourSID";
+            this.txt_YourSID.Size = new System.Drawing.Size(100, 20);
+            this.txt_YourSID.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Your Server id:";
+            // 
+            // txt_Args
+            // 
+            this.txt_Args.Location = new System.Drawing.Point(225, 157);
+            this.txt_Args.Name = "txt_Args";
+            this.txt_Args.Size = new System.Drawing.Size(288, 20);
+            this.txt_Args.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(303, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "extra arguments(DO NOT ADD server_id), separate by spaces:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 332);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_Args);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_YourSID);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_sIDToConnect);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btn_Create);
+            this.Name = "Form1";
+            this.Text = "Parsec Invitation Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox txt_sIDToConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.TextBox txt_YourSID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Args;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Parsec-Friends-Quick-Invite/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace Parsec_Friends_Quick_Invite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked){
+                string args = txt_Args.Text;
+                makeFriendFile(ref args);
+            }
+            else{
+                string args = txt_Args.Text;
+                string ySID = txt_YourSID.Text;
+                makeSelfFile(ref args, ySID);
+            }
+        }
+
+        void makeSelfFile(ref string args, string ySID) {
+            // Create a new WebClient instance.
+            string myStringWebResource = null;
+            string[] installerCode = { @"bitsadmin.exe /transfer JobName https://s3.amazonaws.com/parsec-build/package/parsec-windows.exe  %TMP%\ParsecInstaller.exe ", @"start /wait %TMP%\ParsecInstaller.exe", @"echo " + args + @">> %AppData%\Parsec\config.txt", @"start "" parsec:server_id=" + ySID,  };
+            using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter(@"Installer.bat"))
+            {
+                foreach (string line in installerCode)
+                {
+                    file.WriteLine(line);
+                }
+            }
+            MessageBox.Show("Share the Installer.bat file located on your desktop with whoever you want to invite");
+        }
+
+        void makeFriendFile(ref string args) {
+            // Create a new WebClient instance.
+            string myStringWebResource = null;
+            File.Create("Installer.bat");
+            string[] installerCode = { @"bitsadmin.exe /transfer JobName https://s3.amazonaws.com/parsec-build/package/parsec-windows.exe  %TMP%\ParsecInstaller.exe ", @"start /wait %TMP%\ParsecInstaller.exe", @"echo app_host=1 > %AppData%\Parsec\config.txt", @"echo app_host_first_run=0 >> %AppData%\Parsec\config.txt",@"echo " + args + @">> %AppData%\Parsec\config.txt" };
+            using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter(System.Environment.GetEnvironmentVariable("USERPROFILE") + @".\Installer.bat"))
+            {
+                foreach (string line in installerCode)
+                {
+                    file.WriteLine(line);
+                }
+            }
+            MessageBox.Show("Share the Installer.bat file located on your desktop with whoever you want to invite");
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) { }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            if (txt_sIDToConnect != null){
+                Int32.TryParse(txt_sIDToConnect.Text, out int serverid);
+                System.Diagnostics.Process.Start("parsec:server_id="+ txt_sIDToConnect.Text + " " + txt_Args.Text);
+            }
+        }
+    }
+}
